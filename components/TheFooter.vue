87dc2104220696f13af621c56ed6d9d0092a50e9
KreskoLab <redeezol@gmail.com>
2022-09-05 12:06:15 +0300

    bruh footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,13 +1,20 @@
 <template>
-	<footer class="flex items-center justify-between fixed top-0 bg-blue-gray-100 z-100 w-full h-10 px-4 lg:px-12 text-sm">
-		<div class="flex items-center space-x-2">
+	<footer class="flex items-center justify-between fixed top-0 bg-blue-gray-100 z-100 w-full h-10 px-4 text-xs lg:(px-12 text-sm)">
+		<div>
 			<icon-tabler-phone />
 			<span>+380 63 24 53 347</span>
 		</div>
   
-		<div class="flex items-center space-x-2">
+		<div>
 			<icon-tabler-mail />
 			<span>reply@vitamineria.com.ua</span>
 		</div>
 	</footer>
 </template>
+
+<style scoped>
+	div {
+		@apply
+		flex items-center space-x-1 lg:space-x-2
+	}
+</style>
