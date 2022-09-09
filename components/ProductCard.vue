22806054a7c069f6b57342653a2b5b5dc801abab
Andrii Orlov <andrii.orlov@belkins.io>
2022-09-09 13:27:20 +0300

    fix

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -38,7 +38,7 @@ const decimalPrice = computed(() => props.price.toFixed(2))
 		</NuxtLink>
 
 		<div 
-			v-if="message.length"
+			v-if="message && message.length"
 			class="absolute -right-3 top-2 sm:(top-5 right-0 w-42) transform rotate-20 bg-red-400 py-1 px-3"
 		>
 			<p class="text-sm lg:text-base font-medium text-center">
